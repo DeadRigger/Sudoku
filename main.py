ebad3c678b91718234ebbef2832712bc4d91d8d6
DeadRigger <zemsksan1997@yandex.ru>
2018-12-23 00:24:35 +0300

    алгоритмы создания и проверки судоку

---
+++ main.py
@@ -0,0 +1,204 @@
+import time
+import copy
+import sys
+import random as r
+import pygame.
+
+
+class Sudoku:
+    def __init__(self, base_field):
+        self.complete = base_field
+        self.field = [[None]*9]*9
+        self.solution = [[None]*9]*9
+        self.cycle_generate = r.randrange(10, 30)
+        self.generate()
+
+    def generate(self):
+        for i in range(self.cycle_generate):
+            self.SwapBigRow()
+            self.SwapBigColumn()
+            for j in range(self.cycle_generate):
+                self.SwapSmallRow()
+                self.SwapSmallColumn()
+
+        self.field = copy.deepcopy(self.complete)
+        self.hideNumbers()
+
+    def SwapBigRow(self):
+        row1 = r.randrange(0, 3)
+        row2 = r.randrange(0, 3)
+
+        while row1 == row2:
+            row2 = r.randrange(0, 3)
+
+        for i in range(3):
+            self.complete[row1*3+i], self.complete[row2*3+i] = \
+                self.complete[row2*3+i], self.complete[row1*3+i]
+
+    def SwapBigColumn(self):
+        col1 = r.randrange(0, 3)
+        col2 = r.randrange(0, 3)
+
+        while col1 == col2:
+            col2 = r.randrange(0, 3)
+
+        for c in range(3):
+            for i in range(9):
+                self.complete[i][col1*3 + c], self.complete[i][col2*3 + c] = \
+                    self.complete[i][col2*3 + c], self.complete[i][col1*3 + c]
+
+    def SwapSmallRow(self):
+        row = r.randrange(0, 3)
+        smallRow1 = r.randrange(0, 3)
+        smallRow2 = r.randrange(0, 3)
+
+        while smallRow1 == smallRow2:
+            smallRow2 = r.randrange(0, 3)
+
+        self.complete[row * 3 + smallRow1], self.complete[row * 3 + smallRow2] = \
+            self.complete[row * 3 + smallRow2], self.complete[row * 3 + smallRow1]
+
+    def SwapSmallColumn(self):
+        col = r.randrange(0, 3)
+        smallCol1 = r.randrange(0, 3)
+        smallCol2 = r.randrange(0, 3)
+
+        while smallCol1 == smallCol2:
+            smallCol2 = r.randrange(0, 3)
+
+        for i in range(9):
+            self.complete[i][col * 3 + smallCol1], self.complete[i][col * 3 + smallCol2] = \
+                self.complete[i][col * 3 + smallCol2], self.complete[i][col * 3 + smallCol1]
+
+    def hideNumbers(self):
+        for i in range(9):
+            for c in range(9):
+                if r.randrange(0, 100) > 50:
+                    self.field[i][c] = 0
+
+    def solve(self):
+        solution = copy.deepcopy(self.field)
+        if Sudoku.solveHelper(solution):
+            self.solution = solution
+            return self.solution
+        return None
+
+    def solveHelper(solution):
+        while True:
+            minPossibleValueCountCell = None
+            for rowIndex in range(9):
+                for columnIndex in range(9):
+                    if solution[rowIndex][columnIndex] != 0:
+                        continue
+                    possibleValues = Sudoku.findPossibleValues(rowIndex, columnIndex, solution)
+                    possibleValueCount = len(possibleValues)
+                    if possibleValueCount == 0:
+                        return False
+                    if possibleValueCount == 1:
+                        solution[rowIndex][columnIndex] = possibleValues.pop()
+                    if not minPossibleValueCountCell or \
+                            possibleValueCount < len(minPossibleValueCountCell[1]):
+                        minPossibleValueCountCell = ((rowIndex, columnIndex), possibleValues)
+            if not minPossibleValueCountCell:
+                return True
+            elif 1 < len(minPossibleValueCountCell[1]):
+                break
+        r, c = minPossibleValueCountCell[0]
+        for v in minPossibleValueCountCell[1]:
+            solutionCopy = copy.deepcopy(solution)
+            solutionCopy[r][c] = v
+            if Sudoku.solveHelper(solutionCopy):
+                for r in range(9):
+                    for c in range(9):
+                        solution[r][c] = solutionCopy[r][c]
+                return True
+        return False
+
+    def findPossibleValues(rowIndex, columnIndex, puzzle):
+        values = {v for v in range(1, 10)}
+        values -= Sudoku.getRowValues(rowIndex, puzzle)
+        values -= Sudoku.getColumnValues(columnIndex, puzzle)
+        values -= Sudoku.getBlockValues(rowIndex, columnIndex, puzzle)
+        return values
+
+    def getRowValues(rowIndex, puzzle):
+        return set(puzzle[rowIndex][:])
+
+    def getColumnValues(columnIndex, puzzle):
+        return {puzzle[r][columnIndex] for r in range(9)}
+
+    def getBlockValues(rowIndex, columnIndex, puzzle):
+        blockRowStart = 3 * (rowIndex // 3)
+        blockColumnStart = 3 * (columnIndex // 3)
+        return {
+            puzzle[blockRowStart + r][blockColumnStart + c]
+            for r in range(3)
+            for c in range(3)
+        }
+
+
+def output(a):
+    sys.stdout.write(str(a))
+
+
+def print_field(*args):
+    if not args:
+        output("No solution")
+        return
+
+    for i in range(N):
+        for f in args:
+            for j in range(N):
+                cell = f[i][j]
+                if cell == 0 or isinstance(cell, set):
+                    output('.')
+                else:
+                    output(cell)
+                if (j + 1) % 3 == 0 and j < 8:
+                    output(' |')
+
+                output(' ')
+            output('  ')
+        output('\n')
+
+        if (i + 1) % 3 == 0 and i < 8:
+            for f in args:
+                output("- - - + - - - + - - -   ")
+            output('\n')
+
+
+def check_correct(field):
+    for row in range(N):
+        for col in range(N):
+            if field[row][col] == 0 or \
+                    len(Sudoku.getRowValues(row, field)) != 9 or \
+                    len(Sudoku.getColumnValues(col, field)) != 9 or \
+                    len(Sudoku.getBlockValues(row, col, field)) != 9:
+                return False
+
+    return True
+
+
+N = 9
+
+base_field = [
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [4, 5, 6, 7, 8, 9, 1, 2, 3],
+    [7, 8, 9, 1, 2, 3, 4, 5, 6],
+    [2, 3, 4, 8, 9, 1, 5, 6, 7],
+    [5, 6, 7, 2, 3, 4, 8, 9, 1],
+    [8, 9, 1, 5, 6, 7, 2, 3, 4],
+    [3, 4, 5, 9, 1, 2, 6, 7, 8],
+    [6, 7, 8, 3, 4, 5, 9, 1, 2],
+    [9, 1, 2, 6, 7, 8, 3, 4, 5]
+]
+
+startTime = time.time()
+S = Sudoku(base_field)
+solution = S.solve()
+if solution:
+    if check_correct(solution):
+        print_field(S.field, S.solution, S.complete)
+        print(time.time() - startTime, "seconds")
+    else:
+        print('It\'s no correct decision')
