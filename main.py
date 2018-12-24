ea434f38054752925f41bf9c85c01e41f65cab5c
DeadRigger <zemsksan1997@yandex.ru>
2018-12-24 17:15:13 +0300

    отображение игрового поля

--- main.py
+++ main.py
@@ -2,13 +2,123 @@ import time
 import copy
 import sys
 import random as r
+import pygame
+
+# COLORS #
+
+# Primary
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+# Others
+LIGHT_BLUE = (135, 206, 250)
+GRAY = (175, 175, 175)
+YELLOW = (255, 255, 0)
+
+# Shades
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+TILE_WIDTH = 90
+TILE_SIZE = (TILE_WIDTH, TILE_WIDTH)
+
+BLOCK_WIDTH = 274
+BLOCK_SIZE = (BLOCK_WIDTH, BLOCK_WIDTH)
+
+# Init font for global use
+pygame.font.init()
+FONT = pygame.font.Font(None, 104)
+BOLD_FONT = pygame.font.Font(None, 104)
+BOLD_FONT.set_bold(True)
+
+
+# Class make block 3x3
+class Block:
+    def __init__(self, pos):
+        self.pos = pos
+        self.rect = pygame.Rect(self.pos, BLOCK_SIZE)
+
+        self.surface = pygame.Surface(BLOCK_SIZE).convert()
+        self.surface.fill(BLACK)
+
+        self.tiles = []
+
+
+class Tile:
+
+    def __init__(self, block, pos, number, is_hint=False):
+        self.block = block
+        self.pos = pos
+
+        # Actual answer
+        self.number = number
+
+        # Current provided answer
+        self.entered_number = ""
+
+        # If this tile was given to the player at the start
+        self.is_hint = is_hint
+
+        # Calculate global rectangle pos
+        block_x_pos = self.block.rect.x
+        block_y_pos = self.block.rect.y
+        rect_pos_x = block_x_pos + self.pos[0]
+        rect_pos_y = block_y_pos + self.pos[1]
+        rect_pos = (rect_pos_x,
+                    rect_pos_y)
+
+        self.rect = pygame.Rect(rect_pos, TILE_SIZE)
+
+        self.surface = pygame.Surface(TILE_SIZE).convert()
+
+        self.surface.fill(WHITE)
+
+        if self.is_hint is True:
+            self.draw_number(entry_text=self.number)
+        else:
+            self.draw_number()
+
+    # This will draw self.entered_number unless entry_text is passed
+    # Entry text is passed when a user selects this tile and types
+    def draw_number(self, entry_text=None):
+
+        # Render entry text if given, otherwise render the current entered number
+        if entry_text is None:
+            number_surface = FONT.render(str(self.entered_number), 1, BLACK)
+        else:
+            number_surface = FONT.render(str(entry_text), 1, BLACK)
+
+        surface_center_x = self.surface.get_width() / 2
+        surface_center_y = self.surface.get_height() / 2
+
+        number_place_x = surface_center_x - number_surface.get_width() / 2
+        number_place_y = surface_center_y - number_surface.get_height() / 2
+        number_place = (number_place_x,
+                        number_place_y)
+        self.surface.blit(number_surface, number_place)
+
+    def set_as_hint(self):
+        self.entered_number = self.number
+        self.draw_number(entry_text=self.number)
+        self.is_hint = True
 
 
 class Sudoku:
-    def __init__(self, base_field):
-        self.complete = base_field
-        self.field = [[None]*9]*9
-        self.solution = [[None]*9]*9
+    def __init__(self):
+        self.complete = [
+            [1, 2, 3, 4, 5, 6, 7, 8, 9],
+            [4, 5, 6, 7, 8, 9, 1, 2, 3],
+            [7, 8, 9, 1, 2, 3, 4, 5, 6],
+            [2, 3, 4, 8, 9, 1, 5, 6, 7],
+            [5, 6, 7, 2, 3, 4, 8, 9, 1],
+            [8, 9, 1, 5, 6, 7, 2, 3, 4],
+            [3, 4, 5, 9, 1, 2, 6, 7, 8],
+            [6, 7, 8, 3, 4, 5, 9, 1, 2],
+            [9, 1, 2, 6, 7, 8, 3, 4, 5]
+        ]
+        self.field = [[None] * 9] * 9
+        self.solution = [[None] * 9] * 9
         self.cycle_generate = r.randrange(10, 30)
         self.generate()
 
@@ -31,8 +141,8 @@ class Sudoku:
             row2 = r.randrange(0, 3)
 
         for i in range(3):
-            self.complete[row1*3+i], self.complete[row2*3+i] = \
-                self.complete[row2*3+i], self.complete[row1*3+i]
+            self.complete[row1 * 3 + i], self.complete[row2 * 3 + i] = \
+                self.complete[row2 * 3 + i], self.complete[row1 * 3 + i]
 
     def SwapBigColumn(self):
         col1 = r.randrange(0, 3)
@@ -43,8 +153,8 @@ class Sudoku:
 
         for c in range(3):
             for i in range(9):
-                self.complete[i][col1*3 + c], self.complete[i][col2*3 + c] = \
-                    self.complete[i][col2*3 + c], self.complete[i][col1*3 + c]
+                self.complete[i][col1 * 3 + c], self.complete[i][col2 * 3 + c] = \
+                    self.complete[i][col2 * 3 + c], self.complete[i][col1 * 3 + c]
 
     def SwapSmallRow(self):
         row = r.randrange(0, 3)
@@ -72,7 +182,7 @@ class Sudoku:
     def hideNumbers(self):
         for i in range(9):
             for c in range(9):
-                if r.randrange(0, 100) > 50:
+                if r.randrange(0, 81) < 32:
                     self.field[i][c] = 0
 
     def solve(self):
@@ -136,6 +246,92 @@ class Sudoku:
         }
 
 
+class Game:
+
+    def __init__(self):
+        # Boring pygame init stuff #
+        pygame.init()
+        self.clock = pygame.time.Clock()
+
+        # Game Width
+        self.GW = 843
+        # Game Height
+        self.GH = 843
+        # Tuple of game size
+        self.GS = (self.GW, self.GH)
+
+        flags = pygame.DOUBLEBUF
+        self.game_window = pygame.display.set_mode(self.GS, flags)
+
+        game_surface_size = (self.GW, self.GH)
+        self.game_surface = pygame.Surface(game_surface_size).convert()
+
+        self.tile_spacer = 2
+        self.block_spacer = 5
+
+        self.blocks = []
+
+        self.playing = True
+
+        self.generate_field()
+        for block in self.blocks:
+            for tile in block.tiles:
+                output(tile.number)
+                output(' ')
+            output('\n')
+        self.main()
+
+    def main(self):
+        while self.playing:
+            self.game_surface.fill(BLACK)
+            for block in self.blocks:
+
+                block.surface.fill(BLACK)
+                # draw tiles to block surface
+                for tile in block.tiles:
+                    block.surface.blit(tile.surface, tile.pos)
+                self.game_surface.blit(block.surface, block.pos)
+
+            self.game_window.fill(BLACK)
+            self.game_window.blit(self.game_surface, (0, 0))
+            pygame.display.flip()
+
+            self.clock.tick(60)
+
+    def generate_field(self):
+        S = Sudoku()
+        puzzle = S.field
+
+        x_pos = self.block_spacer
+        y_pos = self.block_spacer
+        for y in range(3):
+            for x in range(3):
+                newpos = (x_pos, y_pos)
+                newblock = Block(newpos)
+
+                tile_grid_x = 0
+                tile_grid_y = 0
+                for tile_y in range(3):
+                    for tile_x in range(3):
+                        newpos = (tile_grid_x, tile_grid_y)
+                        number = puzzle[x * 3 + tile_x][y * 3 + tile_y]
+
+                        newtile = Tile(block=newblock,
+                                       pos=newpos,
+                                       number=number)
+
+                        newblock.tiles.append(newtile)
+
+                        tile_grid_x += (TILE_WIDTH + self.tile_spacer)
+                    tile_grid_y += (TILE_WIDTH + self.tile_spacer)
+                    tile_grid_x = 0
+
+                self.blocks.append(newblock)
+                x_pos += (BLOCK_WIDTH + self.block_spacer)
+            y_pos += (BLOCK_WIDTH + self.block_spacer)
+            x_pos = self.block_spacer
+
+
 def output(a):
     sys.stdout.write(str(a))
 
@@ -145,9 +341,9 @@ def print_field(*args):
         output("No solution")
         return
 
-    for i in range(N):
+    for i in range(9):
         for f in args:
-            for j in range(N):
+            for j in range(9):
                 cell = f[i][j]
                 if cell == 0 or isinstance(cell, set):
                     output('.')
@@ -167,8 +363,8 @@ def print_field(*args):
 
 
 def check_correct(field):
-    for row in range(N):
-        for col in range(N):
+    for row in range(9):
+        for col in range(9):
             if field[row][col] == 0 or \
                     len(Sudoku.getRowValues(row, field)) != 9 or \
                     len(Sudoku.getColumnValues(col, field)) != 9 or \
@@ -178,26 +374,4 @@ def check_correct(field):
     return True
 
 
-N = 9
-
-base_field = [
-    [1, 2, 3, 4, 5, 6, 7, 8, 9],
-    [4, 5, 6, 7, 8, 9, 1, 2, 3],
-    [7, 8, 9, 1, 2, 3, 4, 5, 6],
-    [2, 3, 4, 8, 9, 1, 5, 6, 7],
-    [5, 6, 7, 2, 3, 4, 8, 9, 1],
-    [8, 9, 1, 5, 6, 7, 2, 3, 4],
-    [3, 4, 5, 9, 1, 2, 6, 7, 8],
-    [6, 7, 8, 3, 4, 5, 9, 1, 2],
-    [9, 1, 2, 6, 7, 8, 3, 4, 5]
-]
-
-startTime = time.time()
-S = Sudoku(base_field)
-solution = S.solve()
-if solution:
-    if check_correct(solution):
-        print_field(S.field, S.solution, S.complete)
-        print(time.time() - startTime, "seconds")
-    else:
-        print('It\'s no correct decision')
+Game()
